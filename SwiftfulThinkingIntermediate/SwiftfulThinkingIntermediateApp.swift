416dae078f24d597a5163cce700ddb705b2b1525
Pavel2555 <86742948+Pavel2555@users.noreply.github.com>
2023-06-18 17:37:39 +0300

    Add files via upload

---
+++ SwiftfulThinkingIntermediate/SwiftfulThinkingIntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingIntermediateApp.swift
+//  SwiftfulThinkingIntermediate
+//
+//  Created by Павел Бескоровайный on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingIntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
