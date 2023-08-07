21473148186b526f193ac25e1393c35bc4631226
Pavel Beskorovainyi <beskor1993@icloud.com>
2023-08-07 23:05:32 +0200

    SoundBootcamp player + usage

---
+++ SwiftfulThinkingIntermediate/SoundsBootcamp.swift
@@ -0,0 +1,56 @@
+//
+//  SoundsBootcamp.swift
+//  SwiftfulThinkingIntermediate
+//
+//  Created by Павел Бескоровайный on 07.08.2023.
+//
+
+import SwiftUI
+import AVKit
+
+class SoundManager {
+  static let instance = SoundManager()
+  
+  private init() {}
+  
+  var player: AVAudioPlayer?
+  
+  enum SoundOption: String {
+    case tada
+    case badum
+  }
+  
+  func playSound(sound: SoundOption) {
+    
+    guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
+    
+    do {
+      player = try AVAudioPlayer(contentsOf: url)
+      player?.play()
+    } catch let error {
+      print("Error playing sound. \(error.localizedDescription)")
+    }
+  }
+  
+  
+}
+
+struct SoundsBootcamp: View {
+  
+  var body: some View {
+    VStack(spacing: 40.0) {
+      Button("Play sound 1") {
+        SoundManager.instance.playSound(sound: .tada)
+      }
+      Button("Play sound 2") {
+        SoundManager.instance.playSound(sound: .badum)
+      }
+    }
+  }
+}
+
+struct SoundsBootcamp_Previews: PreviewProvider {
+  static var previews: some View {
+    SoundsBootcamp()
+  }
+}
